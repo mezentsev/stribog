08b30c2ce8e06f61c9a94bd2f4a6221be36d1f1d
v.mezentsev <v.mezentsev@npo-eshelon.ru>
2013-02-28 18:19:24 +0400

    Initial commit

---
+++ stribog/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <QCoreApplication>
+#include <QTextStream>
+#include <QTimer>
+#include <QDebug>
+
+#include "stribog.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QTextStream out(stdout);
+
+    if (argc > 2){
+        // char -> hex
+        char *in = argv[2];
+        int len = 512;
+        try {
+            if ( atoi(argv[1]) == 256)
+                len = 256;
+
+        } catch (const char * msg)
+        {
+            printf("%s", msg);
+            return 0;
+        }
+
+        byte *message = (byte*)in;
+        byte out[64];
+        hash(message, out, len);
+        printf("\nHASH: \n");
+        for (int i = 0; i < (len >> 3); i++)
+        {
+            printf("%.2x", out[i]);
+        }
+    }
+    else
+    {
+        printf("Usage: [bit len (256, 512)] \"[message]\"");
+    }
+
+    return a.exec();
+}
---
+++ stribog/stribog.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-02-20T17:32:48
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = stribog
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    stribog.cpp
+
+HEADERS += \
+    stribog.h
